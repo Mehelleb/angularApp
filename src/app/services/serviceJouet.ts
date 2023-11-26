8bc55e832cc3d251687fe80285f09e4e1cd86c5b
Yacine MEHELLEB <archange34@hotmail.fr>
2023-11-26 20:39:55 +0100

    Ajout d'une méthode pour récupérer un seul jouet pour pouvoir modifier la méthode onLike

--- src/app/services/serviceJouet.ts
+++ src/app/services/serviceJouet.ts
@@ -1,32 +1,46 @@
-import {Injectable} from '@angular/core'
+import { Injectable } from '@angular/core'
 import { Jouet } from 'src/models/Jouet';
 @Injectable({
-    providedIn:"root"
+    providedIn: "root"
 })
 export class ServiceJouet {
-    jouets:Jouet[] = [
+    jouets: Jouet[] = [
         {
-            id:1,
-            name:"Licorne",
-            description:"Très prisée par la petite enfance et spécialement par Alinou.",
-            category:2,
-            price:24,
-            color:"blanc/rose/bleu",
-            likes:250,
-            imageUrl:"https://images.king-jouet.com/6/gu857365_6.jpg"
-            },
+            id: 1,
+            name: "Licorne",
+            description: "Très prisée par la petite enfance et spécialement par Alinou.",
+            category: 2,
+            price: 24,
+            color: "blanc/rose/bleu",
+            likes: 250,
+            imageUrl: "https://images.king-jouet.com/6/gu857365_6.jpg"
+        },
         {
-            id:2,
-            name:"Tortue",
-            description:"Car elle est rigolote, seuls ces petits peuvent la courser !.",
-            category:1,
-            price:60,
-            color:"vert/orange/bleu",
-            likes:120,
-            imageUrl:"https://www.king-jouet.com/fstrz/r/s/images.king-jouet.com/6/gu860464_6.jpg"
-            }
+            id: 2,
+            name: "Tortue",
+            description: "Car elle est rigolote, seuls ces petits peuvent la courser !.",
+            category: 1,
+            price: 60,
+            color: "vert/orange/bleu",
+            likes: 120,
+            imageUrl: "https://www.king-jouet.com/fstrz/r/s/images.king-jouet.com/6/gu860464_6.jpg"
+        }
     ];
-    getAllJouet():Jouet[] {
+    getAllJouet(): Jouet[] {
         return this.jouets;
+    }
+
+    getJouetById(jouetId: number): Jouet {
+        const jouet = this.jouets.find(jouet => jouet.id === jouetId)
+        if (jouet) {
+            return jouet;
+        } else {
+            throw new Error('erreur');
+        }
+    }
+
+    onLikeJouetById(jouetId: number, typeLike: 'like' | 'Unlike'):void{
+        const jouet = this.getJouetById(jouetId)
+        typeLike ==='like'? jouet.likes++ : jouet.likes--;
     }
 }
