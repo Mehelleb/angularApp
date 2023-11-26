415c3bd6da44c41d6b38cd361779215a36bfbb80
Yacine MEHELLEB <archange34@hotmail.fr>
2023-11-26 13:38:37 +0100

    génération de listeJouetComponent et définition d'un tableau de jouets parcouru via ngFor

---
+++ src/app/liste-jouet/liste-jouet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-jouet',
+  templateUrl: './liste-jouet.component.html',
+  styleUrls: ['./liste-jouet.component.scss']
+})
+export class ListeJouetComponent {
+
+}
