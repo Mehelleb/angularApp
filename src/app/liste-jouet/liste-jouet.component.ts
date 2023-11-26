6f5a9adbb7efaeb1702b3598832863e0f4293668
Yacine MEHELLEB <archange34@hotmail.fr>
2023-11-26 15:38:10 +0100

    création de service et injection dans ListeJouetComponent

--- src/app/liste-jouet/liste-jouet.component.ts
+++ src/app/liste-jouet/liste-jouet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Jouet } from 'src/models/Jouet';
+import { ServiceJouet } from '../services/serviceJouet';
 
 @Component({
   selector: 'app-liste-jouet',
@@ -8,28 +9,8 @@ import { Jouet } from 'src/models/Jouet';
 })
 export class ListeJouetComponent implements OnInit {
   jouets!:Jouet[];
+  constructor(private serviceJouet: ServiceJouet){}
   ngOnInit(): void {
-    this.jouets = [
-      {
-        id:1,
-        name:"Licorne",
-        description:"Très prisée par la petite enfance et spécialement par Alinou.",
-        category:2,
-        price:24,
-        color:"blanc/rose/bleu",
-        likes:250,
-        imageUrl:"https://images.king-jouet.com/6/gu857365_6.jpg"
-        },
-        {
-          id:2,
-          name:"Tortue",
-          description:"Car elle est rigolote, seuls ces petits peuvent la courser !.",
-          category:1,
-          price:60,
-          color:"vert/orange/bleu",
-          likes:120,
-          imageUrl:"https://www.king-jouet.com/fstrz/r/s/images.king-jouet.com/6/gu860464_6.jpg"
-          }
-    ]
+    this.jouets = this.serviceJouet.jouets;
   }
 }
